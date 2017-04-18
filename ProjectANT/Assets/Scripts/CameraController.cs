9b54037af1feacee652d71ec86872c6b5970f009
Klemens Strasser <klemens.strasser@gmail.com>
2017-04-18 22:11:56 +0200

    First draft of the game

---
+++ ProjectANT/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public PlayerController player;
+
+	private Vector3 lastPlayerPosition;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float distanceToMove = player.transform.position.y - lastPlayerPosition.y;
+
+		transform.position = new Vector3 (transform.position.x, transform.position.y + distanceToMove, transform.position.z);
+
+		lastPlayerPosition = player.transform.position;
+	}
+}
