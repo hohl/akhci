08099b9a959a3124ef9ae6e9525656c214b124b8
Thorsten Ruprechter <th.ruprechter@gmail.com>
2017-05-05 16:41:23 +0200

    added better background, configurations for android

---
+++ ProjectANT/Assets/Editor/InvisibleTouchable_Editor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+// Touchable_Editor component, to prevent treating the component as a Text object.
+[CustomEditor(typeof(InvisibleTouchable))]
+public class InvisibleTouchable_Editor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		// Do nothing
+	}
+}
