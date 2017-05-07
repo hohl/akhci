fd6059a1b33925f06b4080997e31dc81d7ee6b5d
Thorsten Ruprechter <th.ruprechter@gmail.com>
2017-05-07 13:22:43 +0200

    finalized correct removal of platforms

--- ProjectANT/Assets/Scripts/PlatformController.cs
+++ ProjectANT/Assets/Scripts/PlatformController.cs
@@ -9,7 +9,7 @@ public class PlatformController : MonoBehaviour
 	public float distanceBetweenPlatforms;
 
 	private GameObject startPlatform;
-	public GameObject startTrigger;
+	private GameObject startTrigger;
 
 	private float corridorStart;
 	private float corridorWidth;
@@ -40,6 +40,7 @@ public class PlatformController : MonoBehaviour
 	void Start ()
 	{
 		startPlatform = Resources.Load("Platform") as GameObject;
+		startTrigger = Resources.Load("PlatformTrigger") as GameObject;
 		generator = new SimplePlatformGenerator (GetComponent<AntAlgorithms.AntAlgorithmSimple>());
 		//generator = new RandomPlatformGenerator ();
 		CalculateCorridorValues();
@@ -59,15 +60,15 @@ public class PlatformController : MonoBehaviour
 
 			float x1 = 0.0f;
 			float width1 = next.FirstGap.Position - next.FirstGap.Width / 2;
-			CreatePlatform (x1, width1);
+			GameObject platform = CreatePlatform (x1, width1);
 
-			CreateTrigger (next.FirstGap.Position, next.FirstGap.Width, next.FirstGap.City);
+			CreateTrigger (next.FirstGap.Position, next.FirstGap.Width, next.FirstGap.City, platform);
 
 			float x2 = next.FirstGap.Position + next.FirstGap.Width / 2;
 			float width2 = (next.SecondGap.Position - next.SecondGap.Width / 2) - x2;
 			CreatePlatform (x2, width2);
 
-			CreateTrigger (next.SecondGap.Position, next.SecondGap.Width, next.SecondGap.City);
+			CreateTrigger (next.SecondGap.Position, next.SecondGap.Width, next.SecondGap.City, platform);
 
 			float x3 = next.SecondGap.Position + next.SecondGap.Width / 2;
 			float width3 = 1.0f - x3;
@@ -83,20 +84,25 @@ public class PlatformController : MonoBehaviour
 		corridorWidth = (rightWall.transform.position.x - rightWall.transform.localScale.x / 2.0f) - corridorStart;
 	}
 
-	private void CreatePlatform (float x, float width)
+	private GameObject CreatePlatform (float x, float width)
 	{
 		Vector3 position = new Vector3 (corridorStart + x * corridorWidth + width * corridorWidth / 2, transform.position.y, startPlatform.transform.position.z);
 		GameObject platform = Instantiate (startPlatform, position, transform.rotation);
 		platform.name = String.Format ("Platform(x: {0}, width: {1})", x, width);
 		platform.transform.localScale = new Vector3 (width * corridorWidth, startPlatform.transform.localScale.y, startPlatform.transform.localScale.z);
+		return platform;
 	}
 
-	private void CreateTrigger (float x, float width, City city)
+	private void CreateTrigger (float x, float width, City city, GameObject platform)
 	{
 		Vector3 position = new Vector3 (corridorStart + (x - width / 2) * corridorWidth + width * corridorWidth / 2, transform.position.y, startPlatform.transform.position.z);
 		GameObject trigger = Instantiate (startTrigger, position, transform.rotation);
 		trigger.name = String.Format ("Trigger(pos: {0}, city: {2})", x, width, city == null ? -1 : city.getId());
 		trigger.transform.localScale = new Vector3 (width * corridorWidth, startPlatform.transform.localScale.y, startPlatform.transform.localScale.z);
 		trigger.AddComponent<GapController> ().City = city;
+
+		// this is used to make it easier to remove the triggers
+		// when destroying a platform, the triggers will also be destroyed
+		trigger.transform.parent = platform.transform;
 	}
 }
--- ProjectANT/Assets/Scripts/PlatformDestroyerController.cs
+++ ProjectANT/Assets/Scripts/PlatformDestroyerController.cs
@@ -8,11 +8,13 @@ public class PlatformDestroyerController : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
 	// Update is called once per frame
 	void LateUpdate () {
 		Collider2D[] colliders = new Collider2D[20];
-		int colliderCount = GetComponent<BoxCollider2D>().OverlapCollider(new ContactFilter2D().NoFilter(), colliders);
+		ContactFilter2D filter = new ContactFilter2D();
+		filter.SetLayerMask(LayerMask.GetMask(new string[] { "Default" })); // don't remove walls
+		int colliderCount = GetComponent<BoxCollider2D>().OverlapCollider(filter, colliders);
 		for (int i = 0; i < colliderCount; i++) {
 			Destroy(colliders[i].gameObject);
 		}
