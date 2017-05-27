b7fb6a9c3c517cd31246fbf6d91a8077e0b22ae8
Klemens Strasser <klemens.strasser@gmail.com>
2017-05-27 18:03:32 +0200

    STK: Added an start screen

---
+++ ProjectANT/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController: MonoBehaviour {
+
+	public Button buttonStart;
+	public Button buttonScoreboard;
+
+	// Use this for initialization
+	void Start () {
+		buttonStart.onClick.AddListener(StartGame);
+		buttonScoreboard.onClick.AddListener(ShowScoreboard);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void StartGame() {
+		SceneManager.LoadScene("GameScene");
+	}
+
+	void ShowScoreboard() {
+		
+	}
+}
