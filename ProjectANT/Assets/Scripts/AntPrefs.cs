279042ac24c93851bf5c6267de6e4fb41b01d960
Thorsten Ruprechter <th.ruprechter@gmail.com>
2017-06-17 14:55:49 +0200

    added preferences for project (username)

---
+++ ProjectANT/Assets/Scripts/AntPrefs.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AntPrefs
+{
+	private static AntPrefs instance = null;
+	private static readonly string PREF_USERNAME = "ant_user";
+
+	public static AntPrefs Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = new AntPrefs();
+			}
+			return instance;
+		}
+	}
+
+	public void StoreUsername(string name)
+	{
+		PlayerPrefs.SetString(PREF_USERNAME, name);
+		PlayerPrefs.Save();
+	}
+
+	public string GetUsername()
+	{
+		return PlayerPrefs.GetString(PREF_USERNAME, "Anonymous");
+	}
+}
