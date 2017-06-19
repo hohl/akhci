db7236f1ba87c29ebeaccc286ab96c48c0e26821
Michael Hohl <michael.hohl@student.tugraz.at>
2017-06-19 10:32:13 +0200

    just code clean up

--- ProjectANT/Assets/Scripts/RandomPheromonesPlatformGenerator.cs
+++ ProjectANT/Assets/Scripts/RandomPheromonesPlatformGenerator.cs
@@ -7,29 +7,6 @@ public class RandomPheromonesPlatformGenerator : PheromonesBasedPlatformGenerato
 	{
 	}
 
-	protected override Platform FindNextCities (City startCity)
-	{
-		int startCityIndex = cities.IndexOf (startCity);
-		int firstCityIndex = rand.Next () % cities.Count;
-		int secondCityIndex;
-		do {
-			secondCityIndex = rand.Next() % cities.Count;
-		} while (firstCityIndex == secondCityIndex);
-			
-		City firstCity = cities[firstCityIndex];
-		float firstPheromons = (float) algo.getPheromones().getPheromone(startCityIndex, firstCityIndex);
-		City secondCity = cities[secondCityIndex];
-		float secondPheromons = (float) algo.getPheromones().getPheromone(startCityIndex, secondCityIndex);
-
-		// normalize values! [0..1]
-		firstPheromons = PseudoSafeFloat(firstPheromons);
-		secondPheromons = PseudoSafeFloat (secondPheromons);
-		firstPheromons = -0.3f * firstPheromons / (firstPheromons + secondPheromons) + 0.4f;
-		secondPheromons = 0.3f * secondPheromons / (firstPheromons + secondPheromons) + 0.6f;
-
-		return new Platform (new Gap (firstPheromons, firstCity), new Gap (secondPheromons, secondCity));
-	}
-
 	protected override void SortCities (City unused)
 	{
 		int[] randomValues = new int[cities.Count];
